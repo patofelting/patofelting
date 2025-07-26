f24060cd681181dbd18cf2a662f0fba1e78a72be
patofelting <patofelting@gmail.com>
2025-07-26 16:06:46 -0300

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -4,7 +4,7 @@
 const PRODUCTOS_POR_PAGINA = 6;
 const LS_CARRITO_KEY = 'carrito';
 const CSV_URL = window.SHEET_CSV_URL;
-const PLACEHOLDER_IMAGE = window.PLACEHOLDER_IMAGE || 'https://via.placeholder.com/400x400/7ed957/fff?text=Sin+Imagen';
+
 
 
 // ===============================
