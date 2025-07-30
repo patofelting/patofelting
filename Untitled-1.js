5629369a41037568b588d77ac367176c87d1843d
patofelting <patofelting@gmail.com>
2025-07-29 22:17:59 -0300

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -388,19 +388,8 @@ function actualizarCategorias() {
 }
 // Actualiza la función filtrarProductos
 // Filtrado de productos
-function filtrarProductos() {
-  return productos.filter(p => {
-    const { precioMin, precioMax, categoria, busqueda } = filtrosActuales;
-    const b = busqueda?.toLowerCase() || "";
 
-    return (
-      (precioMin === null || p.precio >= precioMin) &&
-      (precioMax === null || p.precio <= precioMax) &&
-      (categoria === 'todos' || p.categoria === categoria) &&
-      (!b || p.nombre.toLowerCase().includes(b) || p.descripcion.toLowerCase().includes(b))
-    );
-  });
-}
+
 
 
 
